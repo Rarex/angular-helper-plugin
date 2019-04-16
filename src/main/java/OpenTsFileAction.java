97b6c76059763c80f7ae307f4517b485af903e30
Rarex <rarex.dew.accuwork@gmail.com>
2019-04-16 16:48:56 +0300

    Initial commit

---
+++ src/main/java/OpenTsFileAction.java
@@ -0,0 +1,21 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileManager;
+
+public class OpenTsFileAction extends AnAction {
+    public void actionPerformed(AnActionEvent event) {
+        Project project = event.getProject();
+        VirtualFile file = event.getDataContext().getData(PlatformDataKeys.VIRTUAL_FILE);
+        if(project != null && file != null) {
+            String openUrl = file.getUrl().replaceAll("\\.(\\w)*$", ".ts");
+            VirtualFile openVirtualFile = VirtualFileManager.getInstance().findFileByUrl(openUrl);
+            if (openVirtualFile != null) {
+                FileEditorManager.getInstance(project).openFile(openVirtualFile, true);
+            }
+        }
+    }
+}
